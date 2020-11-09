8aa747cdb10a0b43d97395884e439b8246e1c7ec
soleil-taruto <soleil.taruto@gmail.com>
2020-11-09 23:52:58 +0900

    Backup 3

---
+++ DevTools/mask.c
@@ -0,0 +1,53 @@
+/*
+	mask.exe 開始列 長さ
+
+		開始列  ...  0 ～ IMAX
+		長さ    ...  1 ～ IMAX
+
+	----
+	使用例
+
+	TYPE C:\tmp\Backup.log | Grape "ECHO ERRORLEVEL=" | mask 1 24 | TypeGroupBy
+*/
+
+#include "C:\Factory\Common\all.h"
+
+static uint MaskStartIndex;
+static uint MaskLen;
+
+static void RLFS_LineAction(char *line)
+{
+	uint lineLen = strlen(line);
+	uint count;
+
+	for(count = 0; count < MaskLen; count++)
+	{
+		uint index = MaskStartIndex + count;
+
+		if(lineLen <= index)
+			break;
+
+		line[index] = '*';
+	}
+	cout("%s\n", line);
+}
+static void ReadLinesFromStdin(void)
+{
+	char *line;
+
+	while(line = readLine(stdin))
+	{
+		RLFS_LineAction(line);
+		memFree(line);
+	}
+}
+int main(int argc, char **argv)
+{
+	MaskStartIndex = toValue(nextArg());
+	MaskLen        = toValue(nextArg());
+
+	errorCase(!m_isRange(MaskStartIndex, 0, IMAX));
+	errorCase(!m_isRange(MaskLen,        1, IMAX));
+
+	ReadLinesFromStdin();
+}
