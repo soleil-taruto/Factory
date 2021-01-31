0b47083065ed36cf2a57b24accf70194e678a018
soleil-taruto <soleil.taruto@gmail.com>
2021-02-01 08:28:37 +0900

    Backup 20210201082819

--- Tools/Snapshot.c
+++ Tools/Snapshot.c
@@ -1,16 +1,33 @@
+/*
+	Snapshot.exe [/-S]
+*/
+
 #include "C:\Factory\Common\all.h"
 
 int main(int argc, char **argv)
 {
+	int ignoreSubDirFlag = 0;
 	char *path;
+
+	if(argIs("/-S"))
+	{
+		ignoreSubDirFlag = 1;
+	}
 
 	cout("+----------+\n");
 	cout("| Snapshot |\n");
 	cout("+----------+\n");
 
+	if(ignoreSubDirFlag)
+	{
+		cout("###################\n");
+		cout("## Ignore-SubDir ##\n");
+		cout("###################\n");
+	}
+
 	path = dropDirFile();
 
-	coExecute_x(xcout("C:\\app\\Rico\\Snapshot.exe \"%s\"", path));
+	coExecute_x(xcout("C:\\app\\Rico\\Snapshot.exe %s\"%s\"", ignoreSubDirFlag ? "/-S " : "", path));
 
 	memFree(path);
 }
