4b668edb6817312021815f1cddff148acb661eef
soleil-taruto <soleil.taruto@gmail.com>
2021-02-06 00:21:17 +0900

    Backup 20210206002058

---
+++ Rico/CxBuiltBatOnly.c
@@ -0,0 +1,21 @@
+#include "C:\Factory\Common\all.h"
+
+int main(int argc, char **argv)
+{
+	autoList_t *dirs = lsDirs(".");
+	char *dir;
+	uint index;
+
+	foreach(dirs, dir, index)
+	{
+		addCwd(dir);
+		{
+			if(existFile("Built.bat"))
+			{
+				coExecute("cx **");
+			}
+		}
+		unaddCwd();
+	}
+	releaseDim(dirs, 1);
+}
