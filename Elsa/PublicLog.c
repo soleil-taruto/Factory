f11af45bfa388d14ab9bc2f3b86d7343a720f1cb
soleil-taruto <soleil.taruto@gmail.com>
2021-02-10 14:34:14 +0900

    Backup 20210210143357

--- Elsa/PublicLog.c
+++ Elsa/PublicLog.c
@@ -8,31 +8,35 @@
 #define LOG_FILE_02 LOG_FILE "_02.txt"
 #define LOG_FILE_03 LOG_FILE "_03.txt"
 
-#define LOG_FILE_SIZE_MAX 3000000
+#define LOG_FILE_SIZE_MAX 8000000
 
 int main(int argc, char **argv)
 {
 	char *message = untokenize(allArgs(), " "); // g -- memo: allArgs() ÇÕ argv ÇÃêÛÇ¢ÉRÉsÅ[Ç»ÇÃÇ≈ untokenize_xc ÇÕ ng
 
-	if(existFile(LOG_FILE) && LOG_FILE_SIZE_MAX < getFileSize(LOG_FILE))
+	mutex();
 	{
-		if(existFile(LOG_FILE_02))
+		if(existFile(LOG_FILE) && LOG_FILE_SIZE_MAX < getFileSize(LOG_FILE))
 		{
-			removeFileIfExist(
-				LOG_FILE_03
-				);
+			if(existFile(LOG_FILE_02))
+			{
+				removeFileIfExist(
+					LOG_FILE_03
+					);
 
+				moveFile(
+					LOG_FILE_02,
+					LOG_FILE_03
+					);
+			}
 			moveFile(
-				LOG_FILE_02,
-				LOG_FILE_03
+				LOG_FILE,
+				LOG_FILE_02
 				);
 		}
-		moveFile(
-			LOG_FILE,
-			LOG_FILE_02
-			);
+		addLine2File_cx(LOG_FILE, xcout("[%s] %s", c_makeJStamp(NULL, 0), message));
 	}
-	addLine2File_cx(LOG_FILE, xcout("[%s] %s", c_makeJStamp(NULL, 0), message));
+	unmutex();
 
 	memFree(message);
 }
