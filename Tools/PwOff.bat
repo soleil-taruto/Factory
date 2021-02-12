9cff871589cf29c4639bbb1f4d4540580554fd05
soleil-taruto <soleil.taruto@gmail.com>
2021-02-12 10:41:31 +0900

    Backup 20210212104117

--- Tools/PwOff.bat
+++ Tools/PwOff.bat
@@ -1,5 +1,5 @@
 rem シャットダウンします。
-C:\Factory\Elsa\PublicLog.exe PwOff 実行
+C:\Factory\Petra\PublicLog.exe PwOff 実行
 C:\Factory\Tools\wait.exe 30
 IF ERRORLEVEL 1 GOTO END
 shutdown /s /t 30
--- dittybox/oninstall.bat
+++ dittybox/oninstall.bat
@@ -1,4 +1,5 @@
 CALL ff
 CALL Clean
 CALL mkt
+Tools\RDMD.exe /RM tmp_Prime
 Prime 2
