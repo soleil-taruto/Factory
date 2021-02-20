bbb92e10128ef5146a1f0e2bd225edabcaf74c6f
soleil-taruto <soleil.taruto@gmail.com>
2021-02-20 13:53:19 +0900

    Backup 20210220135301

--- Tools/dropCompact.c
+++ Tools/dropCompact.c
@@ -4,63 +4,6 @@
 
 #include "C:\Factory\Common\all.h"
 
-//#define COMPACT_FILE_SIZE_MAX 1000000ui64 // 1MB -- test
-#define COMPACT_FILE_SIZE_MAX 10000000000ui64 // 10GB
-
-static int IsUncompactExt_File(char *file)
-{
-	char *ext = getExt(file);
-
-	return
-		!_stricmp(ext, "gz") ||
-		!_stricmp(ext, "zip");
-}
-static void CompactDirHasHugeFile(char *dir)
-{
-	autoList_t *paths = lss(dir);
-	char *path;
-	uint index;
-
-	addElement(paths, (uint)strx(dir));
-
-	foreach(paths, path, index)
-	{
-		cmdTitle_x(xcout("dropCompact - %u / %u", index, getCount(paths)));
-
-		if(existFile(path) && (IsUncompactExt_File(path) || COMPACT_FILE_SIZE_MAX < getFileSize(path)))
-		{
-			coExecute_x(xcout("Compact.exe /U \"%s\"", path));
-		}
-		else
-		{
-			coExecute_x(xcout("Compact.exe /C \"%s\"", path));
-		}
-	}
-	releaseDim(paths, 1);
-
-	cmdTitle("dropCompact");
-}
-static int HasHugeFile(char *dir)
-{
-	autoList_t *files = lssFiles(dir);
-	char *file;
-	uint index;
-	int ret = 0;
-
-	foreach(files, file, index)
-	{
-		cout("* %s\n", file);
-
-		if(IsUncompactExt_File(file) || COMPACT_FILE_SIZE_MAX < getFileSize(file))
-		{
-			cout("”ñˆ³kI\n");
-			ret = 1;
-			break;
-		}
-	}
-	releaseDim(files, 1);
-	return ret;
-}
 int main(int argc, char **argv)
 {
 	char *path;
@@ -85,31 +28,28 @@ int main(int argc, char **argv)
 			}
 			cout("\n");
 		}
+		error(); // never
 	}
 
-	for(; ; )
 	{
-		cout("+------+\n");
-		cout("| ˆ³k |\n");
-		cout("+------+\n");
-
-		path = dropDirFile();
-
-		if(existDir(path))
+		for(; ; )
 		{
-			if(HasHugeFile(path))
-			{
-				CompactDirHasHugeFile(path);
-			}
-			else
+			cout("+------+\n");
+			cout("| ˆ³k |\n");
+			cout("+------+\n");
+
+			path = dropDirFile();
+
+			if(existDir(path))
 			{
 				coExecute_x(xcout("Compact.exe /C /S:\"%s\"", path));
 			}
+			else // file
+			{
+				coExecute_x(xcout("Compact.exe /C \"%s\"", path));
+			}
+			cout("\n");
 		}
-		else // file
-		{
-			coExecute_x(xcout("Compact.exe /C \"%s\"", path));
-		}
-		cout("\n");
+		error(); // never
 	}
 }
