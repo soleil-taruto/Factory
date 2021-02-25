d659a925a1ff91cf99edeb5c3cb1588c4f8d93db
soleil-taruto <soleil.taruto@gmail.com>
2021-02-26 08:58:40 +0900

    Backup 20210226085754

---
+++ Petra/Lock.c
@@ -0,0 +1,59 @@
+/*
+	Lock.exe ロックファイル (LOCK | UNLOCK)
+*/
+
+#include "C:\Factory\Common\all.h"
+
+int main(int argc, char **argv)
+{
+	char *lockingFile = nextArg();
+
+	lockingFile = makeFullPath(lockingFile);
+
+	cout("ロックファイル：%s\n", lockingFile);
+
+	if(argIs("LOCK"))
+	{
+		cout("ロックしています...\n");
+		LOGPOS_T();
+
+		for(; ; )
+		{
+			int locked = 0;
+
+			mutex();
+			{
+				if(!existFile(lockingFile))
+				{
+					createFile(lockingFile);
+					locked = 1;
+				}
+			}
+			unmutex();
+
+			if(locked)
+				break;
+
+			sleep(2000);
+		}
+		LOGPOS_T();
+		cout("ロックしました。\n");
+	}
+	else if(argIs("UNLOCK"))
+	{
+		cout("ロック解除しています...\n");
+
+		mutex();
+		{
+			removeFileIfExist(lockingFile);
+		}
+		unmutex();
+
+		cout("ロック解除しました。\n");
+		LOGPOS_T();
+	}
+	else
+	{
+		error();
+	}
+}
