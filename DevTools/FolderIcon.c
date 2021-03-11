2548794c00a8627cfc0800d26e631bad7fe2e29d
soleil-taruto <soleil.taruto@gmail.com>
2021-03-11 17:25:18 +0900

    Backup 20210311172516

---
+++ DevTools/FolderIcon.c
@@ -0,0 +1,101 @@
+#include "C:\Factory\Common\all.h"
+
+#define MAPPING_FILE "FolderIcon.conf"
+
+static autoList_t *FIP_Dirs;
+static autoList_t *FIP_IconFiles;
+
+static void LoadFolderIconPairs(void)
+{
+	autoList_t *linesList = readResourceLinesList(MAPPING_FILE);
+	autoList_t *lines;
+	char *line;
+	uint index;
+	uint lines_index;
+
+	FIP_Dirs = newList();
+	FIP_IconFiles = newList();
+
+	foreach(linesList, lines, lines_index)
+	{
+		char *iconFile = getLine(lines, 0);
+
+		errorCase(m_isEmpty(iconFile));
+		errorCase(_stricmp(getExt(iconFile), "ico"));
+		errorCase(!existFile(iconFile));
+
+		foreach(lines, line, index)
+		if(index)
+		{
+			char *dir = line;
+
+			errorCase(m_isEmpty(dir));
+			errorCase(!isFairLocalPath(dir, 0));
+			errorCase(!existDir(dir));
+
+			addElement(FIP_Dirs, (uint)strx(dir));
+			addElement(FIP_IconFiles, (uint)strx(iconFile));
+		}
+	}
+	releaseDim(linesList, 2);
+}
+static void SetFolderIcon(char *dir, char *iconFile)
+{
+	char *iniFile = combine(dir, "desktop.ini");
+
+	if(iconFile)
+	{
+		coExecute_x(xcout("ATTRIB -S -H \"%s\"", iniFile));
+
+		{
+			FILE *fp = fileOpen(iniFile, "wt");
+
+			writeLine(fp, "[.ShellClassInfo]");
+			writeLine_x(fp, xcout("IconResource=%s,0", iconFile));
+
+			fileClose(fp);
+		}
+
+		coExecute_x(xcout("ATTRIB +S +H \"%s\"", iniFile));
+		coExecute_x(xcout("ATTRIB +R \"%s\"", dir));
+	}
+	else
+	{
+		removeFileIfExist(iniFile);
+
+		coExecute_x(xcout("ATTRIB -R \"%s\"", dir));
+	}
+	memFree(iniFile);
+}
+static void ChangeFolderIcons(void)
+{
+	autoList_t *dirs = lsDirs(".");
+	char *dir;
+	uint index;
+
+	eraseParents(dirs);
+
+	foreach(dirs, dir, index)
+	{
+		uint fipIndex = findLineCase(FIP_Dirs, dir, 1);
+
+		if(fipIndex < getCount(FIP_Dirs))
+		{
+			SetFolderIcon(dir, getLine(FIP_IconFiles, fipIndex));
+		}
+		else
+		{
+			SetFolderIcon(dir, NULL);
+		}
+	}
+	releaseDim(dirs, 1);
+}
+static void Main2(void)
+{
+	LoadFolderIconPairs();
+	ChangeFolderIcons();
+}
+int main(int argc, char **argv)
+{
+	Main2();
+}
---
+++ DevTools/ficon.bat
@@ -0,0 +1,1 @@
+FolderIcon.exe
