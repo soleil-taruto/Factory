17a75e42080fa98878d30ff63231327638c208f9
soleil-taruto <soleil.taruto@gmail.com>
2021-03-25 07:01:45 +0900

    Voyager 1 is 22786738037.431 kilometers away from the Sun.

---
+++ Petra/FileList2MD5List.c
@@ -0,0 +1,25 @@
+/*
+	FileList2MD5List.exe ファイルリストファイル 出力ファイル
+*/
+
+#include "C:\Factory\Common\all.h"
+#include "C:\Factory\SubTools\libs\md5Cache.h"
+
+static void FileList2MD5List(char *fileListFile, char *outFile)
+{
+	FILE *rfp = fileOpen(fileListFile, "rt");
+	FILE *wfp = fileOpen(outFile, "wt");
+	char *file;
+
+	while(file = readLine(rfp))
+	{
+		writeLine_x(wfp, md5Cache_makeHexHashFile(file));
+		memFree(file);
+	}
+	fileClose(rfp);
+	fileClose(wfp);
+}
+int main(int argc, char **argv)
+{
+	FileList2MD5List(getArg(0), getArg(1));
+}
