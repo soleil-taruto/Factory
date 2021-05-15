afbb691841ca7b169d4f12ddef997fda49600638
soleil-taruto <soleil.taruto@gmail.com>
2021-05-16 08:44:16 +0900

    Voyager 1 is 22862777386.486 kilometers away from the Sun.

---
+++ Labo/Junk/Tools/DropCopyFile.c
@@ -0,0 +1,29 @@
+#include "C:\Factory\Common\all.h"
+
+int main(int argc, char **argv)
+{
+	char *rFile;
+
+	cout("+--------------------------------------+\n");
+	cout("| 最初にドロップされたファイルの内容を |\n");
+	cout("| ２番目以降にドロップされたファイルに |\n");
+	cout("| 上書きします。                       |\n");
+	cout("+--------------------------------------+\n");
+
+	rFile = dropFile();
+
+	for(; ; )
+	{
+		char *wFile = dropFile();
+
+		cout("< %s\n", rFile);
+		cout("> %s\n", wFile);
+
+		copyFile(rFile, wFile);
+
+		cout("OK\n");
+
+		memFree(wFile);
+	}
+	memFree(rFile);
+}
