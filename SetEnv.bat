4e74430cc25b709526e99eacad8d9b6b89a01051
stackprobe <stackprobes@gmail.com>
2022-12-01 07:27:20 +0900

    Voyager 1 is 23686010336.505 kilometers away from the Sun.

--- SetEnv.bat
+++ SetEnv.bat
@@ -1,7 +1,9 @@
 IF "%FactorySetEnvDone%" == "1" GOTO END
 
+rem 2022
+CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
 rem 2019
-CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+rem CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 rem 2010
 rem CALL "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
