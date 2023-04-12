9b94760c8ab85a6d237f0c16893e3c44d061dcff
soleil.taruto <soleil.taruto@gmail.com>
2023-04-12 14:39:17 +0900

    Voyager 1 is 23879087505.839 kilometers away from the Sun.

---
+++ Tools/b26110.c
@@ -0,0 +1,58 @@
+/*
+	b26110.exe [/L] [/P]
+
+		/L ... 小文字
+		/P ... 表示のみ。(エディタを開かない)
+
+	----
+
+	b26110 == Alphabet(Base-26, A-Z) CX(110) chars
+
+	26 P 110 L 2 == 517.*
+
+	----
+	書式
+
+	{AZCX-YYYYYYYYYYYYYYYYYYYYYY-YYYYYYYYYYYYYYYYYYYYYY-YYYYYYYYYYYYYYYYYYYYYY-YYYYYYYYYYYYYYYYYYYYYY-YYYYYYYYYYYYYYYYYYYYYY}
+	      ~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~
+	              22文字                 22文字                 22文字                 22文字                 22文字
+
+		Y ... Base-26 char
+*/
+
+#include "C:\Factory\Common\all.h"
+#include "C:\Factory\Common\Options\CRandom.h"
+
+#define BASE26_CHARS "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+static char *MakeB26110(void)
+{
+	char *buff = strx("");
+	uint index;
+
+	buff = addLine(buff, "{AZCX-");
+
+	for (index = 0; index < 110; index++)
+	{
+		if (index && index % 22 == 0)
+			buff = addChar(buff, '-');
+
+		buff = addChar(buff, BASE26_CHARS[(uint)getCryptoRand64Mod(26)]);
+	}
+	buff = addChar(buff, '}');
+	return buff;
+}
+int main(int argc, char **argv)
+{
+	char *b26110 = MakeB26110();
+
+	if (argIs("/L"))
+		toLowerLine(b26110);
+
+	cout("%s\n", b26110);
+
+	if (!argIs("/P")) // ? not Print only
+		viewLine(b26110);
+
+	memFree(b26110);
+}
