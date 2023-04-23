da184fa1af06f6386980a807f70a64a200f3fb24
soleil.taruto <soleil.taruto@gmail.com>
2023-04-23 10:01:13 +0900

    Voyager 1 is 23894858768.505 kilometers away from the Sun.

--- GitRelease.bat
+++ GitRelease.bat
@@ -4,5 +4,5 @@ CALL mkt
 
 Petra\GitRelease.exe
 
-C:\apps\GitHubRepositoryFilter\GitHubRepositoryFilter.exe GIT-HUB-REPO-UNSAFE-MOD C:\home\GitHub
+C:\apps\GitHubRepositoryFilter\GitHubRepositoryFilter.exe C:\home\GitHub\Factory
 C:\apps\GitCommit\GitCommit.exe v1s C:\home\GitHub\Factory
